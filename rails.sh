eff13af8b7a50ca31c5be0a5b9dc52df865abd09
Jerry Steele <ticktockhouse@gmail.com>
2015-09-17 16:15:04 +0100

    Got the first psql stuff done - Probably continue to write out *.sql scripts

--- rails.sh
+++ rails.sh
@@ -8,27 +8,16 @@ sudo apt-get -y install postgresql libpq-dev postgresql-contrib
 
 sudo mkdir -p /usr/local/pgsql/data
 sudo chown postgres:postgres /usr/local/pgsql/data
-if [ -d /usr/local/pgsql/data ] ; then
+sudo cat << EOF > altervagrant.sql
+ALTER ROLE vagrant CREATEDB;
+EOF
+sudo -Hu postgres psql -f altervagrant.sql
+if [ -f /usr/local/pgsql/data/pg_hba.conf ] ; then
 	echo "DB already created"
 else
 	sudo -Hu postgres /usr/lib/postgresql/9.3/bin/initdb -D /usr/local/pgsql/data
 	sudo -Hu postgres createuser vagrant
+	sudo su - postgres -c  echo "ALTER ROLE vagrant CREATEDB;" | psql postgres
 fi	
 
-cat << EOF  | su - postgres -c psql
-ALTER ROLE vagrant CREATEDB;
-CREATE EXTENSION dblink;
-IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mydb') THEN
-   RAISE NOTICE 'Database already exists'; 
-ELSE
-   PERFORM dblink_exec('dbname=' || current_database()  -- current db
-                     , 'CREATE DATABASE mydb');
-END IF;
-
-EOF
-
-#cat << EOF | su - postgres -c psql
-#SELECT datname FROM pg_database;
-#EOF
-#|grep sampledb
-
+#echo
