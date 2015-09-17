90ec1397f710998e4731ce5324ca37ab21fe6e07
Jerry Steele <ticktockhouse@gmail.com>
2015-09-17 16:35:55 +0100

    Tidied up & commented

--- rails.sh
+++ rails.sh
@@ -1,23 +1,29 @@
 #!/bin/bash -x
 
+# Update APT and install git
 sudo apt-get -y update
 sudo apt-get -y install git
 
+# Install Postgres
 sudo /usr/sbin/update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 sudo apt-get -y install postgresql libpq-dev postgresql-contrib
 
+# Add postgres dirs
 sudo mkdir -p /usr/local/pgsql/data
 sudo chown postgres:postgres /usr/local/pgsql/data
+
+# Create *.sql script to add alter role of vagrant user
 sudo cat << EOF > altervagrant.sql
 ALTER ROLE vagrant CREATEDB;
 EOF
-sudo -Hu postgres psql -f altervagrant.sql
+
+# If this file exists, we don't need to do the DB init stuff (idempotency)
 if [ -f /usr/local/pgsql/data/pg_hba.conf ] ; then
 	echo "DB already created"
+# Otherwise, do the DB init stuff
 else
 	sudo -Hu postgres /usr/lib/postgresql/9.3/bin/initdb -D /usr/local/pgsql/data
 	sudo -Hu postgres createuser vagrant
-	sudo su - postgres -c  echo "ALTER ROLE vagrant CREATEDB;" | psql postgres
+	sudo -Hu postgres psql -f altervagrant.sql
 fi	
 
-#echo
