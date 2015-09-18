a2ae0d3401efa7433340cf6e7cd1c2d51731686e
Jerry Steele <ticktockhouse@gmail.com>
2015-09-18 12:48:38 +0100

    Think it's working, though can't connect to localhost:3000

--- Vagrantfile
+++ Vagrantfile
@@ -4,5 +4,5 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network "forwarded_port", guest: 3000, host: 3000
-  config.vm.provision :shell, path: "rails.sh"
+  config.vm.provision :shell, path: "rails.sh", privileged: false
 end
--- rails.sh
+++ rails.sh
@@ -2,44 +2,30 @@
 
 # Update APT and install git
 sudo apt-get -y update
-sudo apt-get -y install git curl nodejs libsqlite3-dev 
+sudo apt-get -y install git curl nodejs libsqlite3-dev ruby-dev zlib1g-dev 
 
-# Install Postgres
-sudo /usr/sbin/update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
-sudo apt-get -y install postgresql libpq-dev postgresql-contrib
+sudo gem install bundler
 
-# Add postgres dirs
-sudo mkdir -p /usr/local/pgsql/data
-sudo chown postgres:postgres /usr/local/pgsql/data
+# Sort out the Gemfile to install rails
+cat << EOF > /home/vagrant/Gemfile
+source "https://rubygems.org"
 
-# Create *.sql script to add alter role of vagrant user
-sudo cat << EOF > altervagrant.sql
-ALTER ROLE vagrant CREATEDB;
+gem "rails"
+gem "spring"
+gem "turbolinks"
 EOF
 
-# If this file exists, we don't need to do the DB init stuff (idempotency)
-if [ -f /usr/local/pgsql/data/pg_hba.conf ] ; then
-	echo "DB already initialised"
-# Otherwise, do the DB init stuff
-else
-	sudo -Hu postgres /usr/lib/postgresql/9.3/bin/initdb -D /usr/local/pgsql/data
-	sudo -Hu postgres createuser vagrant
-	sudo -Hu postgres psql -f altervagrant.sql
-fi	
+bundle install
 
-# Create DB as vagrant user
 
-sudo cat << EOF > createdb.sql
-create database sampledb;
-EOF
+# Set up the dir for the rails app
+sudo mkdir /railsapp
+sudo chown vagrant:vagrant /railsapp
+cd /railsapp
 
-sudo -Hu postgres psql -f createdb.sql
-
-#\curl -sSL https://get.rvm.io | bash
-#usermod -G rvm -a vagrant
-#rvm install 1.9.3
-#rvm install 2.1
-#rvm use 2.1 --default
-#gem install bundler
+# Init the rails app
+rails new /railsapp
+# Run the server
+rails s &
 
 echo "Current end of provisioning script. If you got here, it's all over"
